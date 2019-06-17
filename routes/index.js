e3adac3fa8f445e02e2e20f5c5e1d710f44b6c24
KieSP <eightyfour.sp@gmail.com>
2019-06-17 22:02:43 +0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+
+
+var SpotifyWebApi = require('spotify-web-api-node');
+scopes = ['user-read-private', 'user-read-email','playlist-modify-public','playlist-modify-private']
+
+require('dotenv').config();
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_API_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.CALLBACK_URL,
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', (req,res) => {
+  var html = spotifyApi.createAuthorizeURL(scopes)
+  console.log(html)
+  res.send(html+"&show_dialog=true")  
+})
+
+router.get('/callback', async (req,res) => {
+  const { code } = req.query;
+  console.log(code)
+  try {
+    var data = await spotifyApi.authorizationCodeGrant(code)
+    const { access_token, refresh_token } = data.body;
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+
+    res.redirect('http://localhost:3001/home');
+  } catch(err) {
+    res.redirect('/#/error/invalid token');
+  }
+});
+
+router.get('/userinfo', async (req,res) => {
+    try {
+      var result = await spotifyApi.getMe();
+      console.log(result.body);
+      res.status(200).send(result.body)
+    } catch (err) {
+      res.status(400).send(err)
+    }
+});
+
+router.get('/playlists', async (req,res) => {
+  try {
+    var result = await spotifyApi.getUserPlaylists();
+    console.log(result.body);
+    res.status(200).send(result.body);
+  } catch (err) {
+    res.status(400).send(err)
+  }
+
+});
+
+
+module.exports = router;
